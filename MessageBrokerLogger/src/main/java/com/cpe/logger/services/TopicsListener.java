f1fe197c630ee7abe753f6a16525af0b115580a2
Cortoz <corto.deschamps@club-internet.fr>
2023-12-24 11:50:48 +0100

    fixed MQLogger

---
+++ MessageBrokerLogger/src/main/java/com/cpe/logger/services/TopicsListener.java
@@ -0,0 +1,34 @@
+package com.cpe.logger.services;
+
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Service;
+
+import com.cpe.message.model.MessageDTO;
+import com.cpe.user.model.UserDTO;
+
+@Service
+public class TopicsListener {
+
+    private static final Logger log = org.apache.logging.log4j.LogManager.getLogger(TopicsListener.class);
+
+    @Autowired
+    public TopicsListener() {
+    }
+
+    @JmsListener(destination = "com.cpe.spring-app.in.update-user.qmirror")
+    public void receiveUserUpdate(UserDTO user) {
+        log.info("UpdateUserQueue: " + user);
+    }
+
+    @JmsListener(destination = "com.cpe.spring-app.in.add-user.qmirror")
+    public void receiveUserAdd(UserDTO user) {
+        log.info("AddUserQueue: " + user);
+    }
+
+    @JmsListener(destination = "com.cpe.spring-app.in.add-message.qmirror")
+    public void receiveMessageAdd(MessageDTO message) {
+        log.info("AddMsgQueue: " + message);
+    }
+}
